b5af7a899a7ba358afe1c85202ea84958cc14ffa
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2022-02-03 10:59:02 +0100

    XHR + Fetch

--- Network-APIs/01-xml-http-request/api.js
+++ Network-APIs/01-xml-http-request/api.js
@@ -1,15 +1,21 @@
 // AJAX
 // Asynchronous JavaScript and XML
 export function fetchUserById(id) {
-  const xhr = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users/' + id);
-
-  // xhr.onload = function() { }
-  xhr.addEventListener('load', () => {
-    const user = JSON.parse(xhr.responseText);
-    console.log(user);
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users/' + id);
+  
+    // xhr.onload = function() { }
+    xhr.addEventListener('load', () => {
+      if (xhr.status === 200 || xhr.status === 304) {
+        const user = JSON.parse(xhr.responseText);
+        resolve(user);
+      } else {
+        reject(new Error('Bad Status : ' + xhr.status));
+      }
+    });
+  
+    xhr.send();
   });
-
-  xhr.send();
 }
---
+++ Network-APIs/02-fetch/api.js
@@ -0,0 +1,11 @@
+// AJAX
+// Asynchronous JavaScript and XML
+// export function fetchUserById(id) {
+//   return fetch('https://jsonplaceholder.typicode.com/users/' + id)
+//     .then((res) => res.json())
+// }
+
+export async function fetchUserById(id) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  return await res.json();
+}
